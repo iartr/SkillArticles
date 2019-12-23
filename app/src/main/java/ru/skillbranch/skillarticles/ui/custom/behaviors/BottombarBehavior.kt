c6bff1a309b2a70f0cdc9c03a35a39a74c860c63
arilkaev <xaysdiablo@gmail.com>
2019-12-23 18:22:07 +0500

    3: add customs behaviors for bottom bar and submenu

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -1,4 +1,34 @@
 package ru.skillbranch.skillarticles.ui.custom.behaviors
 
-class BottombarBehavior {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+
+class BottombarBehavior<V: View>(context: Context, attrs: AttributeSet): CoordinatorLayout.Behavior<V>(context, attrs) {
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+        child.translationY = maxOf(0f, minOf(child.height.toFloat(), child.translationY + dy))
+    }
 }
