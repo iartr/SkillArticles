b8d7ab75b71d0f3286efc426e4728c043acf2783
arilkaev <xaysdiablo@gmail.com>
2020-02-14 18:47:33 +0500

    4: realize View.setMarginOptionally()

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -1,8 +1,14 @@
 package ru.skillbranch.skillarticles.extensions
 
 import android.view.View
+import android.view.ViewGroup
 
-// TODO: Дз
 fun View.setMarginOptionally(top: Int = 0, right: Int = 0, bottom: Int = 0, left: Int = 0) {
-
+    (layoutParams as ViewGroup.MarginLayoutParams).run {
+        leftMargin = left
+        rightMargin = right
+        topMargin = top
+        bottomMargin = bottom
+    }
+    requestLayout()
 }
