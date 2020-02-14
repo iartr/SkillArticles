3cfd32a9c9e71ec53f1e119707530d068de0467f
arilkaev <xaysdiablo@gmail.com>
2020-02-14 18:51:43 +0500

    4: realize String.indexesOf()

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -1,5 +1,16 @@
 package ru.skillbranch.skillarticles.extensions
 
-fun String.indexesOf(substring: String, ignoreCase: Boolean = true): List<Int> {
-    return emptyList()
+fun String?.indexesOf(substring: String, ignoreCase: Boolean = true): List<Int> {
+    val result = mutableListOf<Int>()
+    if (!this.isNullOrEmpty() && substring.isNotEmpty()) {
+        var index = 0
+        while (index > -1) {
+            index = indexOf(substring, index, ignoreCase)
+            if (index > -1) {
+                result.add(index)
+                index += substring.length
+            }
+        }
+    }
+    return result
 }
