1eab4af155173d97993da4ceee2070efbf254587
arilkaev <xaysdiablo@gmail.com>
2020-02-15 21:38:04 +0500

    4: add PrefManager & PrefDelegate

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
@@ -0,0 +1,45 @@
+package ru.skillbranch.skillarticles.data.delegates
+
+import ru.skillbranch.skillarticles.data.local.PrefManager
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class PrefDelegate<T>(private val defaultValue: T)  {
+    private var value: T? = null
+
+    operator fun provideDelegate(thisRef: PrefManager, prop: KProperty<*>): ReadWriteProperty<PrefManager, T?> {
+        val key = prop.name
+        return object : ReadWriteProperty<PrefManager, T?> {
+            override fun getValue(thisRef: PrefManager, property: KProperty<*>): T? {
+                if (value == null) {
+                    @Suppress("UNCHECKED_CAST")
+                    value = when(defaultValue) {
+                        is Int -> thisRef.preferences.getInt(key, defaultValue as Int) as T
+                        is Long -> thisRef.preferences.getLong(key, defaultValue as Long) as T
+                        is Float -> thisRef.preferences.getFloat(key, defaultValue as Float) as T
+                        is String -> thisRef.preferences.getString(key, defaultValue as String) as T
+                        is Boolean -> thisRef.preferences.getBoolean(key, defaultValue as Boolean) as T
+                        else -> throw IllegalStateException("This type can not be stored into Preferences")
+                    }
+                }
+                return value
+            }
+
+            override fun setValue(thisRef: PrefManager, property: KProperty<*>, value: T?) {
+                with(thisRef.preferences.edit()) {
+                    when (value) {
+                        is String -> putString(key, value)
+                        is Int -> putInt(key, value)
+                        is Boolean -> putBoolean(key, value)
+                        is Long -> putLong(key, value)
+                        is Float -> putFloat(key, value)
+                        else -> throw IllegalStateException("Only primitive types can be stored into Preferences")
+                    }
+                    apply()
+                }
+                this@PrefDelegate.value = value
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -0,0 +1,21 @@
+package ru.skillbranch.skillarticles.data.local
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
+import ru.skillbranch.skillarticles.data.delegates.PrefDelegate
+
+class PrefManager(context: Context) {
+    val preferences: SharedPreferences by lazy { PreferenceManager(context).sharedPreferences }
+
+    var storedBoolean by PrefDelegate(false)
+    var storedString by PrefDelegate("test")
+    var storedInt by PrefDelegate(Int.MAX_VALUE)
+    var storedLong by PrefDelegate(Long.MAX_VALUE)
+    var storedFloat by PrefDelegate(100f)
+
+    fun clearAll() {
+        preferences.edit().clear().apply()
+    }
+}
