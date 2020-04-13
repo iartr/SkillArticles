e0e97aa2d850a28c01eae310695be2c92c769509
arilkaev <xaysdiablo@gmail.com>
2020-04-13 21:18:27 +0500

    7: (!) fix search span

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/markdown/MarkdownTextView.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/markdown/MarkdownTextView.kt
@@ -50,7 +50,7 @@ class MarkdownTextView constructor(
 
     override fun onDraw(canvas: Canvas) {
         if (text is Spanned && layout != null) {
-            canvas.withTranslation {
+            canvas.withTranslation(totalPaddingStart.toFloat(), totalPaddingTop.toFloat()) {
                 searchBgHelper.draw(canvas, text as Spanned, layout)
             }
         }
