a0d368d740851cefd7a60875d85dfea8b3a82b97
arilkaev <xaysdiablo@gmail.com>
2020-05-10 19:50:08 +0500

    8: update models, DataHolder, add EntityGenerator for mock data

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/Date.kt
@@ -3,7 +3,32 @@ package ru.skillbranch.skillarticles.extensions
 import java.text.SimpleDateFormat
 import java.util.*
 
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
+}
+
+fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
+    var time = this.time
+
+    time += when (units) {
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+    }
+    this.time = time
+    return this
+}
+
+enum class TimeUnits {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY;
 }
